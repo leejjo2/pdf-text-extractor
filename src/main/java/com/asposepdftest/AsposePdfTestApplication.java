5fbd2bf46727dccd0e5d2a4b4829a87744ff1c10
choihyunjun <sajun28@naver.com>
2024-12-06 08:49:52 +0900

    push test

--- src/main/java/com/asposepdftest/AsposePdfTestApplication.java
+++ src/main/java/com/asposepdftest/AsposePdfTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AsposePdfTestApplication {
 
+    // 테스트
     public static void main(String[] args) {
         SpringApplication.run(AsposePdfTestApplication.class, args);
     }

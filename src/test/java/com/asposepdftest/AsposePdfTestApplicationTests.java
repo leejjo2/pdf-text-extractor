83f471ce7d4e3a8eb9d6206f3f1a5e7e291bcac5
yoseb <yoseb815@naver.com>
2024-12-05 12:38:26 +0900

    ExcelSaveOptions 엑셀 추출 추가

--- src/test/java/com/asposepdftest/AsposePdfTestApplicationTests.java
+++ src/test/java/com/asposepdftest/AsposePdfTestApplicationTests.java
@@ -1,8 +1,6 @@
 package com.asposepdftest;
 
-import com.aspose.pdf.Document;
-import com.aspose.pdf.TextAbsorber;
-import com.aspose.pdf.TextExtractionOptions;
+import com.aspose.pdf.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,20 +11,35 @@ class AsposePdfTestApplicationTests {
 
     @Test
     void contextLoads() {
-        Document document = new Document("src/main/resources/test.pdf");
+        String fileName = "IDS";
+        Document document = new Document("src/main/resources/"+fileName+".pdf");
 
         TextAbsorber textAbsorber = new TextAbsorber();
 
         TextExtractionOptions options = new TextExtractionOptions(TextExtractionOptions.TextFormattingMode.Pure);
         textAbsorber.setExtractionOptions(options);
 
-        for (int page = 1; page < document.getPages().size(); page++) {
-            document.getPages().get_Item(page).accept(textAbsorber);
-        }
+//        for (int page = 1; page < document.getPages().size(); page++) {
+//            document.getPages().get_Item(page).accept(textAbsorber);
+//        }
+//
+//        String text = textAbsorber.getText();
+//        log.info("text >>>> \n{}", text);
 
-        String text = textAbsorber.getText();
-        log.info("text >>>> \n{}", text);
+        // excel
+//        XlsxSaveOptions
+        ExcelSaveOptions excelSave = new ExcelSaveOptions();
+        excelSave.setFormat(ExcelSaveOptions.ExcelFormat.XLSX);
+        excelSave.setMinimizeTheNumberOfWorksheets(true);
+        document.save(fileName + ".xlsx", excelSave);
 
+        // word
+//        DocSaveOptions docSave = new DocSaveOptions();
+//        docSave.setMode(DocSaveOptions.RecognitionMode.Flow);
+//
+//        docSave.setRelativeHorizontalProximity(2.5f);
+//        docSave.setRecognizeBullets(true);
+//        document.save(fileName + ".doc", docSave);
     }
 
 }
